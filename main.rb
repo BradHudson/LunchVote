c735039059ce55e3fb3d769d5bef8a4676612727
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-06-02 12:19:17 -0400

    Works with dynamo

--- main.rb
+++ main.rb
@@ -1,9 +1,39 @@
 require 'sinatra'
+require 'aws-sdk'
+require 'dotenv'
+Dotenv.load
+
+def dynamo_client
+  Aws::DynamoDB::Client.new(
+  region: ENV['AWS_REGION'],
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  )
+end
+
+def creds
+      {
+        table_name: 'test',
+        item: {
+          "hk" => "g",
+    "name" => "joe"
+  }
+      }
+    end
+
 get '/' do
   "Just Do It"
 end
 
-post "/vote" do
-  @title = params[:title]
-  erb :plan_design
+get "/vote" do
+  # @place = params[:place]
+  # @date = DateTime.now
+ response = dynamo_client.put_item(creds).data rescue nil
+# resp = dynamo_client.get_item(
+#   # required
+#   table_name: "test",
+#   # required
+#   key: {
+#     "hk" => "h", #<Hash,Array,String,Numeric,Boolean,nil,IO,Set>,
+#   })
+#   resp.data
 end
